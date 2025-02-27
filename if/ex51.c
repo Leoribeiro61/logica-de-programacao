6002717158323bd3a115773aa90a94786aa9577e
Leo Ribeiro <leonardo4600@gmail.com>
2025-02-27 17:55:24 -0300

    exercicio 51

--- if/ex51.c
+++ if/ex51.c
@@ -1,7 +1,28 @@
 /*
 51. Faça um algoritmo que leia dois números e mostre qual o maior dos dois . 
 
-ENTRADA: 
-PROCESSAMENTO: 
-SAIDA: 
+ENTRADA: 2 numeros
+PROCESSAMENTO: verificar qual o maior numero
+SAIDA: maior numero
 */
+
+#include <stdio.h>
+
+int main(){
+    int a = 0;
+    int b = 0;
+
+    printf("Digite um valor para 'A': \n");
+    scanf("%d", &a);
+
+    printf("Digite um valor para 'B': \n");
+    scanf("%d", &b);
+
+    if(a > b){
+        printf("O numero %d e maior", a);
+    } else {
+        printf("O numero %d e maior ", b);
+
+    }
+
+}
