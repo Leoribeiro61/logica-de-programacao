93b328e56ad50b48f5ac0f6b8223b5165e32edab
Leo Ribeiro <leonardo4600@gmail.com>
2025-02-27 11:15:16 -0300

    exercicios 42 ao 45

---
+++ if/ex46.c
@@ -0,0 +1,13 @@
+/*
+46. Um hotel cobra R$ 60.00 a diária e mais uma taxa de serviços. A taxa de 
+serviços é de: 
+• R$ 5.50 por diária, se o número de diárias for maior que 15; 
+• R$ 6.00 por diária, se o número de diárias for igual a 15; 
+• R$ 8.00 por diária, se o número de diárias for menor que 15. 
+Construa um algoritmo que mostre o nome e o total da conta de um cliente.
+
+ENTRADA:  numero da conta, saldo, e o tipo de operação a ser realizada e o valor.
+PROCESSAMENTO: somar ou subtrair o valor que foi depositado ou retido 
+SAIDA: o saldo atual da conta 
+
+*/
